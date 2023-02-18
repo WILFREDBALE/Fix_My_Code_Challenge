986aff0758d32626ecaf48f58197dd2c3dd87e91
Wilfred Nankpak Bale <nankpakbalewilfred@gmail.com>
2023-02-18 11:42:50 +0100

    1-print_square.js

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
+
 /*
-    Print a square with the character #
-    
-    The size of the square must be the first argument 
-    of the program.
+  Print a square with the character #
+
+  The size of the square must be the first argument of the program.
 */
 
 // Check if the size argument is provided
 if (process.argv.length <= 2) {
-    console.error("Missing argument");
-    console.error("Usage: ./1-print_square.js <size>");
-    console.error("Example: ./1-print_square.js 8");
-    process.exit(1); // Exit with an error code
+  console.error("Missing argument");
+  console.error("Usage: ./1-print_square.js <size>");
+  console.error("Example: ./1-print_square.js 8");
+  process.exit(1);
 }
 
 // Parse the size argument as an integer
@@ -19,15 +19,17 @@ const size = parseInt(process.argv[2], 10);
 
 // Check if the parsed size is a valid positive integer
 if (isNaN(size) || size <= 0) {
-    console.error(`Invalid size: ${process.argv[2]}`);
-    process.exit(1); // Exit with an error code
+  console.error(`Invalid size: ${process.argv[2]}`);
+  process.exit(1);
 }
 
-// Print the square
-for (let i = 0; i < size; i++) {
-    let row = "";
-    for (let j = 0; j < size; j++) {
-        row += "#";
-    }
-    console.log(row);
+// Print the square of size 10
+const squareSize = 10;
+
+for (let i = 0; i < squareSize; i++) {
+  let row = "";
+  for (let j = 0; j < squareSize; j++) {
+    row += "#";
+  }
+  console.log(row);
 }
